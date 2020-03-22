b92f46061fb7dd8d084b625e013ce8173d890936
Steven Abaco <steve@wizardrywd.com>
2020-03-22 15:11:14 -0500

    Create API Router

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const errorhandler = require('errorhandler');
 const express = require('express');
+const morgan = require('morgan');
+
+const apiRouter = require('./api/api');
 
 const app = express();
 
@@ -9,8 +12,11 @@ const PORT = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
-app.use(errorhandler());
 app.use(morgan('dev'));
+
+app.use('/api', apiRouter);
+
+app.use(errorhandler());
 
 app.listen(PORT, () => {
     console.log('Listening on port: ' + PORT);
