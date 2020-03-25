e8085036355a8e136ef576c02152ade6a9b3a8f6
Steven Abaco <steve@wizardrywd.com>
2020-03-24 22:03:24 -0500

    Add Menu and MenuItem tables

--- migration.js
+++ migration.js
@@ -19,10 +19,20 @@ db.serialize(function() {
             'PRIMARY KEY(`id`), ' +
             'FOREIGN KEY(`employee_id`) REFERENCES `Employee`(`id`) )');
 
+            db.run('CREATE TABLE IF NOT EXISTS `Menu` ( ' +
+            '`id` INTEGER NOT NULL, ' +
+            '`title` TEXT NOT NULL, ' +
+            'PRIMARY KEY(`id`) )');
 
-
-
-
+            db.run('CREATE TABLE IF NOT EXISTS `MenuItem` ( ' +
+           '`id` INTEGER NOT NULL, ' +
+           '`name` TEXT NOT NULL, ' +
+           '`description` TEXT, ' +
+           '`inventory` INTEGER NOT NULL, ' +
+           '`price` INTEGER NOT NULL, ' +
+           '`menu_id` INTEGER NOT NULL, ' +
+           'PRIMARY KEY(`id`), ' +
+           'FOREIGN KEY(`menu_id`) REFERENCES `Menu`(`id`) )');
 
 });
 
